12f0752304ae50f14105ae33cad6267361d2a6dd
尾川貴巳 <>
2021-04-03 22:19:13 +0900

    resourcesを使ったCRUD処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # get 'home/index'
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  get '/users/:id/edit', to: 'users#edit'
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # get '/users', to: 'users#index'
+  # get '/users/new', to: 'users#new'
+  # post '/users', to: 'users#create'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
 end
